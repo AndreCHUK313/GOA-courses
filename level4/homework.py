94e990498be68307945a8c4f1d71565a684c97bb
Andrechuk313 <gishchukandria@gmail.com>
2024-06-02 18:41:18 +0400

    first commit

---
+++ level4/homework.py
@@ -0,0 +1,79 @@
+num1=313
+num2=111
+
+print(num1 + num2)
+
+num3=20
+num4=32
+ 
+print(num3 + num4)
+
+num5=59
+num6=45
+
+print(num5 + num6)
+
+num1=99
+num2=76
+
+print(num1 - num2)
+
+num5=69
+num6=68
+
+print(num5 - num6)
+
+num7=123
+num8=12
+
+print(num7 - num8)
+
+
+
+num3=755
+num4=621
+
+print(num3 * num4)
+
+num5=55
+num6=22
+
+print(num5 * num6)
+
+num7=313
+num8=411
+
+print(num7 * num8 )
+
+
+
+
+num5=126
+num6=2
+ 
+print(num5 // num6)
+
+num8=44
+num9=2
+
+print(num8 // num9 )
+
+num3=313
+num4=1
+
+print(num3 // num4)
+
+num7=56
+num8=2
+
+print(num7 ** num8)
+
+num8=88
+num9=2
+
+print(num8 ** num9)
+
+num1=77
+num2=4
+
+print(num1 ** num2)
