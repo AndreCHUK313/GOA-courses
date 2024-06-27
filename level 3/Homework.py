4aa2885d972e5831e6235b386b65d325dd0ad604
Andrechuk313 <gishchukandria@gmail.com>
2024-06-27 21:07:16 +0400

    fifth

---
+++ level 3/Homework.py
@@ -0,0 +1,20 @@
+name= "andria"
+Lastname="gishchuk"
+hobby="football"
+country="georgia"
+print(name)
+print(name)
+print(name)
+print(name)
+page=320
+book=page+80
+toys=40
+slide=20
+playground=toys*slide
+marbble=10
+odd=5
+even=marbble-odd
+pizza=8
+firends=4
+iate=pizza/friends
+
